49793983b7a4e3acf6beb5c0e11c6cf9bfe3aa59
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-04-14 14:43:17 -0400

    feat: Add data redirect

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,12 @@ import { setupDebugTools } from './utils/debug-tools';
 // Set up debug tools for development
 setupDebugTools();
 
+if (location.pathname === "/data") {
+  location.href = "https://docs.google.com/spreadsheets/d/1q4sn_fiseydSPTENx1-Hblz2yZTDQgk_MF2MnstbAXE";
+}
+
 let container = document.getElementById("app")!;
-let root = createRoot(container)
+let root = createRoot(container);
 root.render(
   <StrictMode>
     <App />
