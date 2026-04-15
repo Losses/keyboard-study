b1b617ed23d188e0ba94a13d35d01b7bc91a2e91
LOSSES Don <1384036+Losses@users.noreply.github.com>
2026-04-14 23:25:21 -0400

    chore: New shortcut

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,16 @@ import { App } from './App';
 import './App.css';
 import { setupDebugTools } from './utils/debug-tools';
 
-// Set up debug tools for development
-setupDebugTools();
-
 if (location.pathname === "/data") {
   location.href = "https://docs.google.com/spreadsheets/d/1q4sn_fiseydSPTENx1-Hblz2yZTDQgk_MF2MnstbAXE";
 }
+
+if (location.pathname === "w1") {
+  location.href = "https://gist.github.com/Losses/8056b3ae3da89dae844fa48df0ea6881"
+}
+
+// Set up debug tools for development
+setupDebugTools();
 
 let container = document.getElementById("app")!;
 let root = createRoot(container);
